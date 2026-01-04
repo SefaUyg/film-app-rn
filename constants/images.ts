4639f2b6f6a1e94cc4900f0caa7a1c950cc0f9f8
SefaUyg <sefauyg32@gmail.com>
2026-01-04 12:50:28 +0300

    second

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,6 @@
 export const images = {
   background: require("@/assets/images/background.png"),
+  logo: require("@/assets/images/logo.png"),
   tabs: require("@/assets/images/tabs.png"),
   ranking: require("@/assets/images/ranking.png")
 };
