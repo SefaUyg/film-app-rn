ab2e51d2fb1c33022f38b2736c0f0f099b022407
SefaUyg <110381806+SefaUyg@users.noreply.github.com>
2026-01-17 19:14:22 +0300

    fourth

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,9 +5,11 @@ import { icons } from '@/constants/icons'
 interface Props {
     placeholder: string;
     onPress?: () => void;
+    value: string;
+    onChangeText: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: Props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-2'>
             <Image source={icons.tabSearch} className="size-5"
@@ -15,8 +17,8 @@ const SearchBar = ({ placeholder, onPress }: Props) => {
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=''
-                onChangeText={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor="#A8B5DB"
                 className='flex-1 ml-2 text-white'
             />
