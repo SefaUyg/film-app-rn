371608dc8807f69aa626f608fcc2999fde0af45f
SefaUyg <110381806+SefaUyg@users.noreply.github.com>
2026-01-18 14:41:25 +0300

    fifth

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import { images } from '@/constants/images';
 import MovieCard from '@/components/MovieCard';
 import { icons } from '@/constants/icons';
 import SearchBar from '@/components/SearchBar';
+import { updateSearchCount } from '@/services/appwrite';
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -21,9 +22,13 @@ const Search = () => {
   }), false)
 
   useEffect(() => {
+
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset()
       }
